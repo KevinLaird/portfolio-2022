616a0be35acddb0b90ee0afab2a41175ad6872dd
Kevin <kevinlaird16@gmail.com>
2022-09-25 14:23:28 -0700

    Created project filter

--- src/components/Project/ProjectFilter.tsx
+++ src/components/Project/ProjectFilter.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ProjectFilter.module.scss';
 
-const ProjectFilter = (): JSX.Element => {
-  let hello;
-  return (
-    <div className={styles.filterContainer}>
-      <div className={styles.verticalLine} />
-      <div className={styles.filterOptions}>
-        <button className={styles.active} type="button">All</button>
-        <button type="button">Games</button>
-      </div>
+const FILTER_DATA = [
+  {
+    id: 1,
+    name: 'All',
+    active: true,
+  },
+  {
+    id: 2,
+    name: 'Games',
+    active: false,
+  },
+  {
+    id: 3,
+    name: 'Websites',
+    active: false,
+  },
+  {
+    id: 4,
+    name: 'Apps',
+    active: false,
+  },
+];
+
+const ProjectFilter = (): JSX.Element => (
+  <div className={styles.filterContainer}>
+    <div className={styles.verticalLine} />
+    <div className={styles.filterOptions}>
+      {FILTER_DATA.length !== 0
+        && FILTER_DATA.map((option) => (
+          <button
+            type="button"
+            key={option.id}
+            className={`${option.active ? styles.active : ''}`}
+          >
+            {option.name}
+          </button>
+        ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default ProjectFilter;
