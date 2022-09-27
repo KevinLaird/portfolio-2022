3e61e19ad253936ca7a5311cdbc35c2b1a294a97
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-09-27 11:36:52 -0700

    Implemented activeFilter selection

--- src/components/Project/ProjectFilter.tsx
+++ src/components/Project/ProjectFilter.tsx
@@ -1,45 +1,62 @@
-import React, { useState } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import styles from './ProjectFilter.module.scss';
 
 const FILTER_DATA = [
   {
     id: 1,
     name: 'All',
-    active: true,
   },
   {
     id: 2,
     name: 'Games',
-    active: false,
   },
   {
     id: 3,
     name: 'Websites',
-    active: false,
   },
   {
     id: 4,
     name: 'Apps',
-    active: false,
   },
 ];
 
-const ProjectFilter = (): JSX.Element => (
-  <div className={styles.filterContainer}>
-    <div className={styles.verticalLine} />
-    <div className={styles.filterOptions}>
-      {FILTER_DATA.length !== 0
-        && FILTER_DATA.map((option) => (
-          <button
-            type="button"
-            key={option.id}
-            className={`${option.active ? styles.active : ''}`}
-          >
-            {option.name}
-          </button>
-        ))}
+const ProjectFilter = (): JSX.Element => {
+  // Set the default filter on load to "All"
+  const defaultActiveElement = FILTER_DATA.find(
+    (filterOption) => (filterOption.name === 'All'),
+  );
+
+  // Save the filter ID as the default active state filter
+  const [activeFilterId, setActiveFilterId] = useState(
+    defaultActiveElement ? defaultActiveElement.id : 1,
+  );
+
+  // Change the active filter
+  const handleFilterUpdate = (event: SyntheticEvent): void => {
+    const target = event.target as HTMLInputElement;
+    const filterSelection = parseInt(target.value, 10);
+    setActiveFilterId(filterSelection);
+  };
+
+  return (
+    <div className={styles.filterContainer}>
+      <div className={styles.verticalLine} />
+      <div className={styles.filterOptions}>
+        {FILTER_DATA.length !== 0
+          && FILTER_DATA.map((option) => (
+            <button
+              type="button"
+              key={option.id}
+              value={option.id}
+              className={`${option.id === activeFilterId ? styles.active : ''}`}
+              onClick={handleFilterUpdate}
+            >
+              {option.name}
+            </button>
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProjectFilter;
