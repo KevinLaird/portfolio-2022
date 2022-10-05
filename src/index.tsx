1ecf88c12fb6298c460cdb91a951d96a82beb61f
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-10-05 15:50:32 -0700

    Updated ReactDOM.render to support react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './global-css/main.scss';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
