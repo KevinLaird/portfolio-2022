999f7bf83d4ffcd40b2b6b2edb19a4acf517b786
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-10-09 16:40:45 -0700

    Set up index for browser router usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './global-css/main.scss';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
 );
