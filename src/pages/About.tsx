5b30095e03e5a38233c9baf45f437b0c4087dfd8
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-10-09 16:41:23 -0700

    Added 2 pages to web app

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tabs from '../components/UI/Tabs';
+
+const About = (): JSX.Element => (
+  <>
+    <h1>hello world</h1>
+    About
+    <Tabs />
+  </>
+);
+
+export default About;
