9545d9e3a72a8c133f2d5a7c17eb451261afd75e
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-10-09 18:11:18 -0700

    Currently implementing Tabs component

--- src/components/UI/Tabs.tsx
+++ src/components/UI/Tabs.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Tabs.module.scss';
 
-const Tabs = (): JSX.Element => (
-  <div>Tabs</div>
-);
+const Tabs = (): JSX.Element => {
+  const [isActive, setIsActive] = useState('');
+
+  return (
+    <div className={styles.tabContainer}>
+      <button type="button" className={`${styles.tab} ${styles.active}`}>
+        active Element
+      </button>
+      <button type="button" className={styles.tab}>Inactive Element</button>
+      <button type="button" className={styles.tab}>Inactive Element</button>
+      <button type="button" className={styles.tab}>Inactive Element</button>
+    </div>
+  );
+};
 
 export default Tabs;
