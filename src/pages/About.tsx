f9f4abdfe541ec7ac3354fb7337dc07105ee63b4
KevinLaird <kevinlaird16@gmail.com>
2022-12-05 15:08:36 -0800

    Designing About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RxCaretRight } from 'react-icons/rx';
+import styles from './About.module.scss';
+
 import ContentTabs from '../components/UI/Tabs/ContentTabs';
 
 const DUMMY_JSX: { id: string, title: string, content: string; }[] = [
@@ -12,14 +15,111 @@ const DUMMY_JSX: { id: string, title: string, content: string; }[] = [
     title: 'Title 2',
     content: 'Content',
   },
+  {
+    id: 't3',
+    title: 'Title 2',
+    content: 'Content',
+  },
+  {
+    id: 't4',
+    title: 'Title 2',
+    content: 'Content',
+  },
+  {
+    id: 't5',
+    title: 'Title 2',
+    content: 'Content',
+  },
+  {
+    id: 't6',
+    title: 'Title 2',
+    content: 'Content',
+  },
 ];
 
-const About = (): JSX.Element => (
-  <>
-    <h1>hello world</h1>
-    About
-    <ContentTabs content={DUMMY_JSX} />
-  </>
-);
+const About = (): JSX.Element => {
+  const [tabSelect, setTabSelect] = useState(0);
+
+  const updateTabSelectionHandler = (): void => {
+    if (tabSelect === 0) {
+      setTabSelect(1);
+    } else {
+      setTabSelect(0);
+    }
+  };
+
+  const bio = (
+    <div className={styles.textContent}>
+      <h2>Lorem, ipsum.</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis alias assumenda numquam consectetur veniam accusamus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis neque praesentium mollitia temporibus officia.</p>
+    </div>
+  );
+
+  const resume = (
+    <>
+      <div className={styles.resume}>
+        <div className={styles.resumeItem}>
+          <h3 className={styles.resumeDate}>
+            Jan 2021 - Mar 2022
+          </h3>
+          <div className={styles.resumePosition}>
+            <h3>Cloud Support Engineer</h3>
+            <p>Hostwinds LLC</p>
+          </div>
+        </div>
+        <div className={styles.resumeItem}>
+          <h3 className={styles.resumeDate}>
+            Jan 2021 - Mar 2022
+          </h3>
+          <div className={styles.resumePosition}>
+            <h3>Cloud Support Engineer</h3>
+            <p>Hostwinds LLC</p>
+          </div>
+        </div>
+      </div>
+      <button type="button" className={styles.resumeDownload}>
+        Download
+        <RxCaretRight size={28} />
+      </button>
+    </>
+  );
+
+  return (
+    <>
+      <div className={styles.container}>
+        <img
+          src="./assets/profile-picture.jpg"
+          alt="profile"
+          className={styles.profile}
+        />
+        <div className={styles.content}>
+          <div className={styles.tab}>
+            <button
+              type="button"
+              className={
+                tabSelect === 0 ? styles.active : ''
+              }
+              onClick={updateTabSelectionHandler}
+            >
+              Bio
+            </button>
+            <button
+              type="button"
+              className={
+                tabSelect === 1 ? styles.active : ''
+              }
+              onClick={updateTabSelectionHandler}
+            >
+              Resume
+            </button>
+          </div>
+          {tabSelect === 0 ? bio : resume}
+        </div>
+      </div>
+      <ContentTabs content={DUMMY_JSX} />
+    </>
+  );
+};
 
 export default About;
