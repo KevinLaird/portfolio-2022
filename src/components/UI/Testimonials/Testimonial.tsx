a47d8a6f3880d03fb4a7dd7e25bb690a7feb00f1
KevinLaird <kevinlaird16@gmail.com>
2023-07-13 14:28:58 -0700

    Updated name of person in Testimonial

--- src/components/UI/Testimonials/Testimonial.tsx
+++ src/components/UI/Testimonials/Testimonial.tsx
@@ -15,7 +15,7 @@ const Testimonial = (): JSX.Element => (
         <div className={styles.profileContainer}>
           <img
             className={styles.profile}
-            src="./assets/testimonial/abe.jpg"
+            src="./assets/testimonial/aviva.png"
             alt="profile"
           />
         </div>
@@ -25,8 +25,8 @@ const Testimonial = (): JSX.Element => (
             <p>{'Kevin is a talented designer with an exceptional work ethic. He is passionate about what he does, and it comes through not only in the high quality of work that he produces, but in the positive relationships he forges with the people he collaborates with. As a team player, Kevin is a good listener and compromiser, but he also isn\'t afraid to advocate for a position he strongly believes in. I am confident that Kevin will make a positive and valuable addition to any company or project he contributes to.'}</p>
           </div>
           <div>
-            <h2>Abraham Avnisan</h2>
-            <h3>University of Washington Professor</h3>
+            <h2>Aviva Avnisan</h2>
+            <h3>UW Professor of Interactive Media Design</h3>
           </div>
         </div>
       </div>
@@ -43,7 +43,7 @@ const Testimonial = (): JSX.Element => (
         <img src="./assets/testimonial/audrena.jpg" alt="nav bubble audrena" />
       </li>
       <li className={styles.active}>
-        <img src="./assets/testimonial/abe.jpg" alt="nav bubble audrena" />
+        <img src="./assets/testimonial/aviva.png" alt="nav bubble audrena" />
       </li>
       <li>
         <img src="./assets/testimonial/wanda.jpg" alt="nav bubble audrena" />
