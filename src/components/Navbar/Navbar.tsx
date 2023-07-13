013329941107eb04dff6b5a335c9d0fc1e7fc32b
KevinLaird <kevinlaird16@gmail.com>
2023-07-13 14:30:38 -0700

    Navbar now checks if it's a hashlink or a link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,26 +4,39 @@ import { HashLink } from 'react-router-hash-link';
 import styles from './Navbar.module.scss';
 import NavbarFooter from './NavbarFooter';
 
-const NAVBAR_ITEMS: { id: string, name: string, link: string; }[] = [
+const NAVBAR_ITEMS: { id: string, name: string, link: string; hashlink: boolean; }[] = [
   {
     id: '01',
     name: 'About',
     link: '/about',
+    hashlink: false,
+  },
+  {
+    id: '02',
+    name: 'Projects',
+    link: '/#projects',
+    hashlink: true,
   },
 ];
 
 const Navbar = (): JSX.Element => (
   <header className={styles.container}>
     <nav className={styles.navContainer}>
-      <HashLink to="/#projects" className={styles.logoBox}>
+      <Link to="/" className={styles.logoBox}>
         <img className={styles.logo} src="./assets/logo.png" alt="logo" />
         Kevin Laird
-      </HashLink>
+      </Link>
       <ul className={styles.items}>
         {NAVBAR_ITEMS.length !== 0
           && NAVBAR_ITEMS.map((item) => (
             <li key={item.id}>
-              <Link to={item.link}>{item.name}</Link>
+              {item.hashlink
+                ? (
+                  <HashLink to="/#projects" className={styles.logoBox}>
+                    Projects
+                  </HashLink>
+                )
+                : <Link to={item.link}>{item.name}</Link>}
             </li>
           ))}
       </ul>
